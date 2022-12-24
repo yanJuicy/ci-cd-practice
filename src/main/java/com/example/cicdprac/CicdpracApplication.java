7eb59844684463159435c42b1e0ada030d4569ac
Yanjoo <hanjub3@gmail.com>
2022-12-24 10:50:42 +0900

    initial commit

---
+++ src/main/java/com/example/cicdprac/CicdpracApplication.java
@@ -0,0 +1,13 @@
+package com.example.cicdprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdpracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdpracApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cicdprac/CicdpracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicdprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdpracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
